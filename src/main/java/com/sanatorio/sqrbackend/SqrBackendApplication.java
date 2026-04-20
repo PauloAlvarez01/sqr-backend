fa2f77a6e3692f199f567739ebf4306f826d750e
Paulo Alvarez <paulo.manuel.alvarez@gmail.com>
2026-04-20 19:36:15 -0300

    Initial commit: Estructura base y Docker configurado

---
+++ src/main/java/com/sanatorio/sqrbackend/SqrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sanatorio.sqrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqrBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqrBackendApplication.class, args);
+    }
+
+}
