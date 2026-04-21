8e70892dd7764460698b4685ccbde7cd8bc28c63
Paulo Alvarez <paulo.manuel.alvarez@gmail.com>
2026-04-20 21:14:34 -0300

    Entidad Especialidad creada, se comprueba conexion a base de datos

---
+++ src/main/java/com/sanatorio/sqrbackend/entities/Especialidad.java
@@ -0,0 +1,20 @@
+package com.sanatorio.sqrbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "especialidad")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Especialidad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 25, unique = true)
+    private String nombreEspecialidad;
+}
