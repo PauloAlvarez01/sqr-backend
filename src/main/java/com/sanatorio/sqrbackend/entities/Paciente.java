ee427170fa34181d7e7e7238d05fbb5f729d942b
Paulo Alvarez <paulo.manuel.alvarez@gmail.com>
2026-04-22 20:38:39 -0300

    Se configura entidad Paciente para registros de auditoría

--- src/main/java/com/sanatorio/sqrbackend/entities/Paciente.java
+++ src/main/java/com/sanatorio/sqrbackend/entities/Paciente.java
@@ -2,12 +2,18 @@ package com.sanatorio.sqrbackend.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "paciente")
+@EntityListeners(AuditingEntityListener.class) // para autocompletar los campos de auditoria
 @Getter
 @Setter
 @NoArgsConstructor
@@ -39,15 +45,19 @@ public class Paciente {
     @Column(nullable = false)
     private LocalDate fechaNacimientoPaciente;
 
-    @Column(nullable = false, length = 25)
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 25)
     private String creadoPor;
 
-    @Column(nullable = false)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
     private LocalDateTime fechaCreacion;
 
+    @LastModifiedBy
     @Column(nullable = false, length = 25)
     private String modificadoPor;
 
+    @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime fechaModificacion;
 
